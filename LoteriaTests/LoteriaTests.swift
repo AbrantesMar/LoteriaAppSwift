0623e318d08ac7c63ad14d300b0cc4ae063a9bc9
AbrantesMar <abs.marcio@gmail.com>
2021-08-04 19:06:54 -0300

    Initial Commit

---
+++ LoteriaTests/LoteriaTests.swift
@@ -0,0 +1,36 @@
+//
+//  LoteriaTests.swift
+//  LoteriaTests
+//
+//  Created by Márcio Abrantes on 04/08/2021.
+//  Copyright © 2021 Márcio Abrantes. All rights reserved.
+//
+
+import XCTest
+@testable import Loteria
+
+class LoteriaTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
